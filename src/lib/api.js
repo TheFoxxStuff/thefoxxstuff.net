3f5d30efa7672d3cd6317f17399fcfc833215942
Aytal Popov <aytal.popov2002@yandex.ru>
2023-01-20 01:57:46 +0900

    s

--- src/lib/api.js
+++ src/lib/api.js
@@ -20,13 +20,53 @@ export async function getAllCategoriesWithPosts() {
 }
 
 export async function getAllRel() {
-    const query = `*[_type == 'music']{...,author -> {trackname, tracknumber, trackduration}} | order(publishedAt desc)`;
+    const query = `*[_type == 'music']{...,author -> {trackname, tracknumber, trackduration}} | order(released desc)`;
     const data = await useSanityClient().fetch(query);
     return data;
 }
 
+// export async function getAllArts() {
+//     const query = `*[_type == 'art']{..., "images": images[]->{slug, image, ...}} | order(publishedAt desc)`;
+//     const data = await useSanityClient().fetch(query);
+//     return data;
+// }
+
 export async function getAllArts() {
-    const query = `*[_type == 'art']`;
+    const query = `*[_type == 'art']{..., "images": images[]->{slug, image, ...}, date}|order(date desc)`;
     const data = await useSanityClient().fetch(query);
     return data;
 }
+
+
+
+
+// export async function getGalaryMainPage() {
+//     const query = `*[_type == 'gallarymainpage']{
+//         piccc->{
+//           _id,
+//           title,
+//           ...,
+//           image{
+//             asset->
+//           }
+//         }
+//       }`;
+//     const data = await useSanityClient().fetch(query);
+//     return data;
+// }
+
+export async function getGalaryMainPage() {
+    const query = `*[_type == 'gallarymainpage']{"Big1": pic1-> {title, ...},"RightUp1": pic2-> {title, ...},"RightUp2": pic3-> {title, ...},"RightDown1": pic4-> {title, ...},"RightDown2": pic5-> {title, ...}} | order(publishedAt desc)`;
+    const data = await useSanityClient().fetch(query);
+    return data;
+}
+
+// export async function getGalaryMainPage() {
+//     const query = `*[_type == 'gallarymainpage']{piccc}`;
+//     const data = await useSanityClient().fetch(query);
+//     const picccId = data[0].piccc._ref
+//     const picccQuery = `*[_id == $picccId]`;
+//     const picccParams = { picccId };
+//     const piccc = await useSanityClient().fetch(picccQuery, picccParams)
+//     return piccc;
+// }
