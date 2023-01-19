a3235f1b9e764f8e7ba8e6de5b6aad749afbc48c
Aytal Popov <aytal.popov2002@yandex.ru>
2023-01-20 02:08:05 +0900

    asd

--- src/lib/api.js
+++ src/lib/api.js
@@ -7,17 +7,17 @@ import { useSanityClient } from "astro-sanity";
 // }
 
 
-export async function getAllPosts() {
-    const query = `*[_type == 'post']{..., "categoryData": categories[]->{slug, title},author -> {name, slug, image, bio}} | order(publishedAt desc)`;
-    const data = await useSanityClient().fetch(query);
-    return data;
-}
+// export async function getAllPosts() {
+//     const query = `*[_type == 'post']{..., "categoryData": categories[]->{slug, title},author -> {name, slug, image, bio}} | order(publishedAt desc)`;
+//     const data = await useSanityClient().fetch(query);
+//     return data;
+// }
 
-export async function getAllCategoriesWithPosts() {
-    const query = `*[_type == 'category']{"posts": *[_type == "post" && references(^._id)] | order(publishedAt desc), ...}`;
-    const data = await useSanityClient().fetch(query);
-    return data;
-}
+// export async function getAllCategoriesWithPosts() {
+//     const query = `*[_type == 'category']{"posts": *[_type == "post" && references(^._id)] | order(publishedAt desc), ...}`;
+//     const data = await useSanityClient().fetch(query);
+//     return data;
+// }
 
 export async function getAllRel() {
     const query = `*[_type == 'music']{...,author -> {trackname, tracknumber, trackduration}} | order(released desc)`;
