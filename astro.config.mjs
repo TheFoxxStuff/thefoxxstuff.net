a1a8c46b2b79e32670ff23b8dae4ebebdc8b473e
Aytal Popov <aytal.popov2002@yandex.ru>
2023-01-24 00:49:30 +0900

    aff

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import sanity from "astro-sanity";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://thefoxxstuff.net',
-    integrations: [mdx(), sitemap(), sanity({
-        projectId: 'ndh6wvwa',
-        dataset: 'production',
-        apiVersion: '2021-03-25',
-        useCdn: true,
-    })]
+  site: 'https://thefoxxstuff.net',
+  integrations: [mdx(), sitemap(), sanity({
+    projectId: 'ndh6wvwa',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    useCdn: true
+  })],
+  output: "server",
+  adapter: vercel()
 });
