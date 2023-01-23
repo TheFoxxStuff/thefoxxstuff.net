7143af1a4bdbc4f4010e1f0dca38216a0c60e554
Aytal Popov <aytal.popov2002@yandex.ru>
2023-01-24 02:25:04 +0900

    s

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://thefoxxstuff.net',
+    site: 'https://thefoxxstuff-net.pages.dev',
     integrations: [mdx(), sitemap(), sanity({
         projectId: 'ndh6wvwa',
         dataset: 'production',
