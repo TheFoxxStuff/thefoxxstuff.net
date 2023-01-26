fe34e8f276a4af4373956f8a606d163aaef0b957
Aytal Popov <aytal.popov2002@yandex.ru>
2023-01-27 01:48:45 +0900

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,16 @@ import sitemap from '@astrojs/sitemap';
 import sanity from "astro-sanity";
 import cloudflare from "@astrojs/cloudflare";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://thefoxxstuff-net.pages.dev',
-  integrations: [mdx(), sitemap(), sanity({
-    projectId: 'ndh6wvwa',
-    dataset: 'production',
-    apiVersion: '2021-03-25',
-    useCdn: true
-  })],
-  output: 'server' // adapter: cloudflare()
-  ,
-  adapter: netlify()
+    site: 'https://thefoxxstuff-net.pages.dev',
+    integrations: [mdx(), sitemap(), sanity({
+        projectId: 'ndh6wvwa',
+        dataset: 'production',
+        apiVersion: '2021-03-25',
+        useCdn: true
+    })],
+    output: 'server' // adapter: cloudflare()
+        ,
+    adapter: cloudflare()
 });
