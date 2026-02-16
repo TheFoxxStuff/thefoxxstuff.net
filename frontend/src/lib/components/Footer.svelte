ed824512f2ae234d0f98c6abca6017037f60c93e
Aytal Popov <42153584+createandchoose@users.noreply.github.com>
2026-02-17 03:24:00 +0900

    test

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -7,7 +7,7 @@
 
 <footer class="mx-auto w-full max-w-[828px] flex items-center justify-between mb-[60px] my-[20px] p-[14px_30px] bg-[--w5] rounded-[8px]">
   <div class="text-[--w60] text-[14px]">
-    © TheFoxxStuff {currentYear}
+    © TheFoxxStuff TEST {currentYear}
   </div>
 
   <div class="flex flex-row gap-2 items-center">
