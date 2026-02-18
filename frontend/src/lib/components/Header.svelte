53ec796796ddd6f856f273bf3702ee609158572c
Aytal Popov <42153584+createandchoose@users.noreply.github.com>
2026-02-18 18:35:59 +0900

    test

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -12,6 +12,7 @@
     { href: '/arts', label: 'Arts' },
     { href: '/links', label: 'Links' },
     { href: '/about', label: 'About' },
+    { href: '/test', label: 'test' },
   ];
 
   const isActive = (href, pathname) => href === '/' ? pathname === '/' : pathname.startsWith(href);
